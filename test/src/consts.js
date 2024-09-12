4e86e160e6c131e8d56a78eb8c7cc9b5e5d7bb16
avigyabb <avigyabb@stanford.edu>
2024-09-11 19:43:21 -0700

    docker files

--- test/src/consts.js
+++
@@ -1,3 +0,0 @@
-// export const REACT_APP_API_URL = 'https://amboralabsservice.com/' // prod
-export const REACT_APP_API_URL = 'http://127.0.0.1:8000/' // dev
-// export const REACT_APP_API_URL = 'https://amboralabsservice.com:4343/' // prod staging
--- test_backend/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # 👈 Add include here
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapi.urls')),
-    path('vectordb/', include('vectordb.urls')),
-]
